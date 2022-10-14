1940839a7cfec4015fcdf97cf2de26ad318539e6
Phani547 <115761756+Phani547@users.noreply.github.com>
2022-10-14 20:20:50 +0530

    Assignment task
    Assignment task

---
+++ src/app/app.component.ts
@@ -0,0 +1,67 @@
+import { Component, Input ,OnInit} from '@angular/core';
+import {publicApiService} from '../app/services/publicapi.service';
+import{User} from './class/user';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  providers:[publicApiService]
+})
+export class AppComponent  implements OnInit {
+  
+  title = 'DemoApp';
+  fName:string;
+  lName:string;
+  email:string="";
+  users:User[]=[];
+  user:User= new User();
+
+  constructor(private apiService : publicApiService)
+  {
+  }
+  
+  ngOnInit()
+  {
+    // get all users data
+    this.apiService.getData().subscribe(
+      data=>{
+        this.users = data.data;
+      }
+    )
+
+  }
+  
+  getusers()
+  {
+    this.apiService.getData().subscribe(
+      data=>{
+        this.users = data.data;
+        alert("Fetched Successfully!");
+      }
+    )
+  }
+  // Submit user data
+  submit()
+  {
+    this.user.email = this.email;
+    this.user.first_name = this.fName;
+    this.user.last_name = this.lName;
+   if(this.user.email !="" && this.user.first_name !="" && this.user.last_name !="")
+   {
+    this.apiService.submitData(this.user).subscribe(
+      data=>
+      {
+       console.log("better");
+       alert("sucess");
+       this.email ="";
+       this.fName ="";
+       this.lName="";
+      }
+    )
+  }
+  else{
+    alert("Please fill up missing field(s)!");
+  }
+  }
+}
---
+++ src/app/services/publicapi.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {Observable} from'rxjs';
+import{User} from '../class/user';
+
+@Injectable()
+
+export class publicApiService
+{
+   posturl = "https://reqres.in/api/users";//"https://gorest.co.in/public/v2/users";
+   getUrl = "https://reqres.in/api/users?page=1";
+  constructor(private httpClient : HttpClient)
+  {
+  }
+    submitData(user:User):Observable<any>
+    {
+      return this.httpClient.post(this.posturl,user);
+    }
+    getData():Observable<any>
+    {
+        return this.httpClient.get(this.getUrl);
+    }
+  }   
